8a7b3a74a4a4a4298ac518f9227f4f7dc678c9b8
Enrico Walther <enrico.walther@kleinhain.de>
2024-12-01 11:53:24 +0100

    feat: aoc 2024 day 1

--- advent-of-code-solutions/src/advent_of_code_2024.rs
+++ advent-of-code-solutions/src/advent_of_code_2024.rs
@@ -14,7 +14,15 @@ pub fn advent_of_code(solutions: &mut BTreeMap<u32, Box<dyn AoCSolution>>) -> Re
 
     // Add Day 1
     let day_01_src: &'static [u8] = include_bytes!("solutions/aoc2024/day01_impl.rs");
-    solutions.insert(1, Box::new(aoc2024::Day01::new(1, YEAR, "Foo", day_01_src)));
+    solutions.insert(
+        1,
+        Box::new(aoc2024::Day01::new(
+            1,
+            YEAR,
+            "Historian Hysteria",
+            day_01_src,
+        )),
+    );
 
     Ok(())
 }
